043be0aee2ebbeb7bdc8df34d0883ae557839194
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2020-09-09 13:28:52 +0300

    Create 2.14.2.js

---
+++ 2.14.2.js
@@ -0,0 +1,12 @@
+script>
+    'use strict'
+    function checkAge(age) {
+    return = (age > 18) ? true : confirm('Родители разрешили?'); 
+    }
+</script>
+<script>
+    'use strict'
+    function checkAge(age) {
+    return = (age > 18) || confirm('Родители разрешили?'); 
+    }
+</script>
