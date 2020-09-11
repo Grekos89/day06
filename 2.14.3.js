4895c3b843650303afa5f51b96ae4cb424f62d39
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2020-09-11 18:12:04 +0300

    Update 2.14.3.js

--- 2.14.3.js
+++ 2.14.3.js
@@ -1,9 +1,18 @@
 <script>
-    function min(a, b) {
-        if(a < b) {
-        return a 
-        } else {
-        return b    
+    function pow(x, n) {
+        let result = x;
+        for (let i = 1; i < n; i++) { // вообще не понял смысла этой строчки от куда i и для чего она? 
+            result *= x;
         }
+        return result;
+        }
+    }
+    let x = prompt("x?", '');
+    let n = prompt("n?", '');
+    if (n < 1) {
+        alert(`Степень ${n} не поддедживается, используйте натуральное число`);
+
+    } else {
+        alert(pow(x, n));
     }
 </script>
